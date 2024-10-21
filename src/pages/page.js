5489f56d8c813c7b30aebddb1ad5b02e829043eb
Bhautik Rank <bhautik.r.r@gmail.com>
2024-10-21 21:49:17 +0530

    First Noted

---
+++ src/pages/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroSection from 'pages/sections/Hero.section'
+import Navbar from 'components/navbar'
+
+const Page = () => {
+    return (
+        <>
+            <Navbar />
+            <HeroSection />
+        </>
+    )
+}
+
+export default Page
